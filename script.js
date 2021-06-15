a56f5474ec748d2ccd83bb8182916d1fac44d191
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-06-15 12:28:37 +0200

    added 2 objects with items to populate the second field

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 (function() {
     console.warn('Script linked correctly to web page');
+
+    let super_pro = {
+        heavyBag: '40 kg cheap model',
+        gloves: 'Cheap training gloves',
+        bands: 'Cheap boxing bands'
+    }
+
+    let leone_1947 = {
+        heavyBag: '50 kg professional heavy bag',
+        gloves: 'Intermediate sparring gloves',
+        bands: 'Semi-elasting boxing bands',
+        other: 'Reflex ball'
+    }
 })();
